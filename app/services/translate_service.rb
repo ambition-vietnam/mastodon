652170c84c80907f3fa16a52ea42d45b8c2d2c42
manhcuong06 <nguyencuong945@gmail.com>
2018-02-26 14:22:01 +0700

    add_translate_feature

--- app/services/translate_service.rb
+++ app/services/translate_service.rb
@@ -5,6 +5,8 @@ require 'google/cloud/translate'
 
 class TranslateService < BaseService
   def call(text, target)
+    return text if ENV['GOOGLE_APPLICATION_CREDENTIALS'].nil?
+
     credentials = File.read(ENV['GOOGLE_APPLICATION_CREDENTIALS'])
     project_id = JSON.parse(credentials)['project_id']
     translate = Google::Cloud::Translate.new project: project_id
